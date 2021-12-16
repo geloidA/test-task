48cb0744f315412172efc02b5e87b064cc8a1a3c
Aleksandr <alexkolpak2003@gmail.com>
2021-12-16 18:30:31 +0500

    create entity Card and delete dict shuffleDecks

---
+++ TestTask/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckSorter
+{
+    public class Card
+    {
+        public readonly CardSuit Suit;
+        public readonly CardValue Value;
+
+        public Card(CardSuit suit, CardValue value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+    }
+}
--- TestTask/Deck.cs
+++ TestTask/Deck.cs
@@ -5,9 +5,9 @@ namespace DeckSorter
 {
     public class Deck
     {
-        private readonly IReadOnlyList<(CardSuit, CardValue)> cards;
+        private readonly IReadOnlyList<Card> cards;
 
-        public IEnumerable<(CardSuit, CardValue)> Cards
+        public IEnumerable<Card> Cards
         {
             get
             {
@@ -18,16 +18,16 @@ namespace DeckSorter
 
         public Deck()// создаёт стандартную колоду карт (52 штуки)
         {
-            var cards = new List<(CardSuit, CardValue)>();
+            var cards = new List<Card>();
 
             foreach (var suit in (CardSuit[])Enum.GetValues(typeof(CardSuit)))
                 foreach (var value in (CardValue[])Enum.GetValues(typeof(CardValue)))
-                    cards.Add((suit, value));
+                    cards.Add(new Card(suit, value));
 
             this.cards = cards;
         }
 
-        public Deck(IReadOnlyList<(CardSuit, CardValue)> cards)
+        public Deck(IReadOnlyList<Card> cards)
         {
             this.cards = cards;
         }
